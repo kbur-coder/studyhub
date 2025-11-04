2a9929508b53ad8495dea054dca86d0129f474ee
kbur-coder <kbur2968@gmail.com>
2025-11-04 23:37:02 +0500

    Добавил Постгрес и регистрацию

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.project_list, name='project_list'),
-    path('create/', views.project_create, name='project_create'),
+    path('create/', views.create_project, name='create_project'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import redirect, render
-
-from .forms import ProjectForm
-
+from django.shortcuts import render, redirect
 from .models import Project
+from .forms import ProjectForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def project_list(request):
-    projects = Project.objects.all().order_by("-created_at")
-    return render(request, 'projects/project_list.html', {'projects':projects})
+    projects = Project.objects.all()
+    return render(request, 'projects/project_list.html', {'projects': projects})
 
-
-def project_create(request):
+def create_project(request):
     if request.method == 'POST':
         form = ProjectForm(request.POST)
         if form.is_valid():
@@ -17,4 +16,31 @@ def project_create(request):
             return redirect('project_list')
     else:
         form = ProjectForm()
-    return render(request, 'projects/project_create.html', {'form': form})
+    return render(request, 'projects/create_project.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('project_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'projects/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('project_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'projects/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('project_list')
