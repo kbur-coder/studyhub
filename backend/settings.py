7cfd4ba8f97c69b631c8705815e016a1a7381573
kbur-coder <kbur2968@gmail.com>
2025-11-17 07:12:02 +0500

    Fix DEBUG setting for local development - enable static files

--- backend/settings.py
+++ backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*!*$iqjy2kx2l8v!0htdwt&=8#c+#ff40^#$mw_67fm7l-btc0')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else ['*']
 
